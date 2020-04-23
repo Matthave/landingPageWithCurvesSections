450c5ab841a7d6f71258382916f99a23b5a2e214
Matt <matthave@gmail.com>
2020-04-23 14:40:56 +0200

    First Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <p className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noopener noreferrer'>Frontend Mentor</a>.
+          Coded by <a href="http://www.matthave.pl/" target="_blank" rel='noopener noreferrer'>Matthave</a>.
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
